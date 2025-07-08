3fe1950aea60827399aa425f68c0b30bba86cef9
pepijn-devries <peppi2@gmail.com>
2025-07-08 18:56:09 +0200

    Updated product details and meta data functions for new API

---
+++ R/cms_catalogue_entry.R
@@ -0,0 +1,28 @@
+#' @export
+cms_get_client_info <- function() {
+  ci <- .try_online({
+    "https://stac-dta.marine.copernicus.eu/clients-config-v1" |>
+      httr2::request() |>
+      httr2::req_perform()
+  }, "client-info-page")
+  if (is.null(ci)) return(NULL) else return(httr2::resp_body_json(ci))
+}
+temp <- function() {
+  products <-
+    "https://s3.waw4-1.cloudferro.com/mdl-metadata-dta/dataset_product_id_mapping.json" |>
+    httr2::request() |>
+    httr2::req_perform()
+  products |>
+    httr2::resp_body_json()
+}
+## Python definitions TODO this is just not for my own reference. Remove later
+# root_metadata_url := catalogue["stacRoot"]
+# dataset_product_mapping_url=catalogue["idMapping"]
+# stac_catalogue_url=catalogue["stac"]
+# catalogue in mds_config["catalogues"]
+# 
+# url = f"{stac_url}/{product_id}/product.stac.json"
+# product_json = connection.get_json_file(url)
+# product_collection = pystac.Collection.from_dict(
+#   product_json
+# )
--- R/cms_product_metadata.r
+++ R/cms_product_metadata.r
@@ -1,17 +1,29 @@
-#' Obtain meta data (Deprecated)
+#' Obtain product meta data
 #'
-#' `r lifecycle::badge('deprecated')`This function is no longer supported by Copernicus
-#' Marine. Use `cms_product_details()` instead.
+#' `r lifecycle::badge('experimental')`TODO
 #'
 #' @include cms_download_subset.r
-#' @param product Ignored.
-#' @param type Ignored.
-#' @returns Returns `NULL` as this service is no longer provided by Copernicus.
+#' @param product Ignored. TODO
+#' @param type Deprecated and ignored.
+#' @returns TODO
 #' @rdname cms_product_metadata
 #' @name cms_product_metadata
+#' @examples
+#' cms_product_metadata("GLOBAL_ANALYSISFORECAST_PHY_001_024")
 #' @author Pepijn de Vries
 #' @export
-cms_product_metadata <- function(product, type = c("list", "xml")) {
-  .Deprecated("cms_product_details")
-  return (NULL)
+cms_product_metadata <- function(product, type, ...) {
+  details <- cms_product_details(product)
+  if (is.null(details)) return (NULL)
+  links     <- lapply(details$links, as.data.frame) |> dplyr::bind_rows()
+  item      <- links |> dplyr::filter(.data$rel == "item") |> dplyr::pull("href")
+  meta_url  <- paste(attr(details, "stac_url"), product, item, sep = "/")
+  meta_data <- .try_online({
+    meta_url |>
+      httr2::request() |>
+      httr2::req_perform()
+  }, "meta-data-page")
+  if (is.null(meta_data)) return(NULL) else {
+    return(httr2::resp_body_json(meta_data))
+  }
 }
