2e769437a23a9a057491310fdbb365a454ef535a
pepijn-devries <peppi2@gmail.com>
2025-07-15 00:51:36 +0200

    adaded 'prefix' arg for faster results

--- R/cms_download_native.R
+++ R/cms_download_native.R
@@ -11,6 +11,8 @@
 #' to select specific files. For instance if `pattern = "2022/06/"`, only files for the
 #' year 2022 and the month June will be listed (assuming that the file path is structured as such, see
 #' examples)
+#' @param prefix A `character` string. A prefix to be added to the search path of the files.
+#' Only the matching file (info) is downloaded (generally faster then using `pattern`)
 #' @param progress A `logical` value. When `TRUE` a progress bar is shown.
 #' @param ... Ignored
 #' @returns Returns `NULL` invisibly.
@@ -20,7 +22,7 @@
 #'   cms_list_native_files(
 #'     product       = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
 #'     layer         = "cmems_mod_glo_phy_anfc_0.083deg_PT1H-m",
-#'     pattern       = "2022/06/"
+#'     prefix        = "2022/06/"
 #'   )
 #' 
 #' ## Use 'pattern' to download a file for a specific day:
@@ -28,15 +30,17 @@
 #'     destination   = tempdir(),
 #'     product       = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
 #'     layer         = "cmems_mod_glo_phy_anfc_0.083deg_PT1H-m",
+#'     prefix        = "2022/06/",
 #'     pattern       = "m_20220630"
 #'   )
 #' }
 #' @rdname cms_download_native
 #' @export
-cms_download_native <- function(destination, product, layer, pattern, progress = TRUE, ...) {
+cms_download_native <- function(destination, product, layer, pattern, prefix, progress = TRUE, ...) {
   if (missing(pattern)) pattern <- ""
-
-  file_list <- cms_list_native_files(product, layer, pattern)
+  if (missing(prefix)) prefix <- ""
+  
+  file_list <- cms_list_native_files(product, layer, pattern, prefix)
   
   for (i in nrow(file_list)) {
     path_out <- unlist(strsplit(file_list$Key[[i]], "/"))[-1:-2]
@@ -88,11 +92,10 @@ cms_download_native <- function(destination, product, layer, pattern, progress =
 
 #' @rdname cms_download_native
 #' @export
-cms_list_native_files <- function(product, layer, pattern, ...) {
+cms_list_native_files <- function(product, layer, pattern, prefix, ...) {
   if (missing(pattern)) pattern <- ""
   if (missing(layer)) layer <- ""
-  s3_info <- .preprocess_native(product, layer)
-  
+  s3_info <- .preprocess_s3(product, layer, "native_href")
   if (is.null(s3_info)) return(NULL)
   
   with(s3_info, {
@@ -100,7 +103,9 @@ cms_list_native_files <- function(product, layer, pattern, ...) {
       path,
       region = "",
       bucket = bucket,
-      base_url = endpoint
+      base_url = endpoint,
+      prefix = paste0(path, "/", prefix),
+      max = Inf
     ) |>
       dplyr::filter(grepl(pattern, .data$Key, perl = TRUE)) |>
       dplyr::mutate(base_url = endpoint)
@@ -108,24 +113,26 @@ cms_list_native_files <- function(product, layer, pattern, ...) {
     dplyr::as_tibble()
 }
 
-.preprocess_native <- function(product, layer, ...) {
+.preprocess_s3 <- function(product, layer, what, ...) {
   services <-
     cms_product_services(product)
   if (is.null(services)) return(NULL)
   services <-
     services |>
     dplyr::filter(startsWith(.data$id, layer))
-  native_url <- services$native_href
+  service_url <- services[[what]]
+  service_url <- unlist(service_url)
+  if (length(service_url) > 1) stop("Unexpected multiple URLs")
   
   ## regex used in python toolbox to parse url
   parse_regex <- "^(http|https):\\/\\/([\\w\\-\\.]+)(:[\\d]+)?(\\/.*)"
   
-  m <- gregexpr(parse_regex, native_url, perl = TRUE)
+  m <- gregexpr(parse_regex, service_url, perl = TRUE)
   m_start  <- attr(m[[1]], "capture.start")
   m_length <- attr(m[[1]], "capture.length")
   fragments <-
     mapply(function(start, len) {
-      substr(native_url, start, start + len - 1)
+      substr(service_url, start, start + len - 1)
     }, start = m_start, len = m_length)
   segments <- strsplit(fragments[4], "/") |> unlist()
   
