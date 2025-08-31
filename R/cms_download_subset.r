c531bd0c8b23dcfde6b3116fb2d878a4efe0c39e
pepijn-devries <peppi2@gmail.com>
2025-08-31 12:27:00 +0200

    Updated subset documentation

--- R/cms_download_subset.r
+++ R/cms_download_subset.r
@@ -1,12 +1,13 @@
 #' Subset and download a specific marine product from Copernicus
 #'
-#' `r lifecycle::badge('questioning')` Subset and download a specific marine product from Copernicus.
-#' You need to register an account
-#' at <https://data.marine.copernicus.eu> before you can use this function.
+#' `r lifecycle::badge('experimental')` Subset and download a specific marine product
+#' from Copernicus.
 #'
 #' @include cms_login.r
 #' @inheritParams cms_login
-#' @param destination TODO deprecate (data is loaded into memory)
+#' @param destination `r lifecycle::badge("deprecated")` This argument is deprecated.
+#' Data is no longer written to a file but loaded as a [stars::st_as_stars()] object
+#' into memory.
 #' @param product An identifier (type `character`) of the desired Copernicus marine product.
 #' Can be obtained with [`cms_products_list`].
 #' @param layer The name of a desired layer within a product (type `character`). Can be obtained with [`cms_product_details`].
@@ -19,33 +20,34 @@
 #' @param verticalrange A `vector` with two elements (minimum and maximum)
 #' numerical values for the depth of the vertical layers (if any). Note that values below the
 #' sea surface needs to be specified as negative values.
-#' @param overwrite TODO deprecate
-#' @param progress TODO
+#' @param overwrite `r lifecycle::badge("deprecated")` This argument is deprecated.
+#' Data is no longer written to a file but loaded as a [stars::st_as_stars()] object
+#' into memory.
+#' @param progress A logical value. When `TRUE` (default) progress is reported to the console.
+#' Otherwise, this function will silently proceed.
 #' @param crop On the server, the data is organised in chunks. The subset
 #' will download chunks that overlap with the specified ranges, but often
 #' covers a larger area. When `crop = TRUE` (default), the data will be cropped
 #' to the specified `region`. If set to `FALSE` all downloaded data will be returned.
-#' @returns TODO
+#' @returns Returns a [stars::st_as_stars()] object.
 #' @rdname cms_download_subset
 #' @name cms_download_subset
 #' @examples
-#' \dontrun{
-#' destination <- tempfile("copernicus", fileext = ".nc")
+#' if (interactive()) {
+#'   destination <- tempfile("copernicus", fileext = ".nc")
 #'
-#' ## Assuming that Copernicus account details are provided as `options`
-#' cms_download_subset(
-#'   destination   = destination,
-#'   product       = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
-#'   layer         = "cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m",
-#'   variable      = c("uo", "vo"),
-#'   region        = c(-1, 50, 10, 55),
-#'   timerange     = c("2025-01-01 UTC", "2025-01-02 UTC"),
-#'   verticalrange = c(0, -2)
-#' )
-#' 
-#' mydata <- stars::read_stars(destination)
-#' 
-#' plot(mydata["vo"])
+#'   mydata <- cms_download_subset(
+#'     destination   = destination,
+#'     product       = "GLOBAL_ANALYSISFORECAST_PHY_001_024",
+#'     layer         = "cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m",
+#'     variable      = c("uo", "vo"),
+#'     region        = c(-1, 50, 10, 55),
+#'     timerange     = c("2025-01-01 UTC", "2025-01-02 UTC"),
+#'     verticalrange = c(0, -2)
+#'   )
+#'
+#'   my_data
+#'   plot(mydata["vo"])
 #' }
 #' @author Pepijn de Vries
 #' @export
