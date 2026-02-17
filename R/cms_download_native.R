12abe0e92fe17c77a8a031a6de25edded83e489e
Pepijn de Vries <pepijn-devries@users.noreply.github.com>
2026-02-18 00:04:51 +0100

    Work in progress (#136)
    * Updated NEWS
    
    * Some small fixes and improved test coverage
    
    * Rerendered README
    
    * Version bump for CRAN release
    
    * Fixed test
    
    * Added rhub workflow
    
    * Fix for https://github.com/pepijn-devries/CopernicusMarine/issues/127
    
    * Added proxy vignette
    
    * Try to fix failing test-coverage workflow
    
    * Some fixes to vignette and cross references
    
    * Added login for functions that download from s3 storage
    
    * Updated NEWS file
    
    * Improved test coverage
    
    * Version bump for CRAN release. And added html and R files in vignettes dir to .buildignore
    
    * Don't make 'translate' vignette self-contained. Otherwise, it will embed the >2Mb gif animation
    
    * Don't run proxy-vignette chunks if password is not set...
    
    * And only if the HDF5 driver is present
    
    * added namespace
    
    * forgot some eval args
    
    * Explicitly retrieve an pass variable names when reading proxy object when missing
    
    * Added lifecycle badge to `cms_zarr_proxy()`
    
    * Added lifecycle badge to `cms_native_proxy()` as well

--- R/cms_download_native.R
+++ R/cms_download_native.R
@@ -164,7 +164,7 @@ cms_list_native_files <- function(product, layer, pattern, prefix, max = Inf, ..
 
 #' Get a proxy stars object from a native service
 #' 
-#' The advantage of
+#' `r lifecycle::badge('experimental')` The advantage of
 #' [`stars_proxy` objects](https://r-spatial.github.io/stars/articles/stars2.html#stars-proxy-objects),
 #' is that they do not contain any data. They are therefore fast to handle
 #' and consume only limited memory. You can still manipulate the object
